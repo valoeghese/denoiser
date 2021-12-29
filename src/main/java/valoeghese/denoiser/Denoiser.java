68eabd1cb4f9a44ae756c0cd56da7f499050144d
valoeghese <valoeghesemods@gmail.com>
2021-12-29 13:05:48 +1300

    gtg for now I'll finish this later

--- src/main/java/valoeghese/denoiser/Denoiser.java
+++ src/main/java/valoeghese/denoiser/Denoiser.java
@@ -4,14 +4,36 @@ import net.fabricmc.api.ModInitializer;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
+import java.util.Random;
+
 public class Denoiser implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
 	public static final Logger LOGGER = LogManager.getLogger("denoiser");
+	public static final Random RANDOM = new Random();
 
 	@Override
 	public void onInitialize() {
-		LOGGER.info("Wassup losers");
+		LOGGER.info(funnyMessage[RANDOM.nextInt(funnyMessage.length - 1)]);
 	}
+
+	private static String[] funnyMessage = {
+			"Why are you using this mod",
+			"Why",
+			"Valoeghese - doing the worldgen no one asked for since 2018",
+			"\"Mod compatibility has gone from difficult to lmao\"",
+			"Origin Valley 2 Electic Boogaloo",
+			"Calculating the matrix vertices using the chonkamajig beardifier",
+			"Writing funny startup messages instead of making the mod!",
+			"No one asked for this",
+			"Actually BOP kinda did but they decided it's not worth the effort",
+			"If a datapacker saw this their eyes would explode",
+			"If a datapacker saw this their ears would probably explode too",
+			"Lambda, builder, lambda, FACTORY! Lambda, builder, lambda, FACTORY!",
+			"Mixin :)",
+			"Mixin :(",
+			"Gosh damned datapackifying refactors >:(",
+			"Stop reading the source code and experience these messages yourself you coward! ;)"
+	};
 }
--- src/main/java/valoeghese/denoiser/mixin/FuckNoiseCavesInParticular.java
+++ src/main/java/valoeghese/denoiser/mixin/FuckNoiseCavesInParticular.java
@@ -3,17 +3,25 @@ package valoeghese.denoiser.mixin;
 import net.minecraft.world.level.levelgen.NoiseSampler;
 import net.minecraft.world.level.levelgen.TerrainInfo;
 import net.minecraft.world.level.levelgen.blending.Blender;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(NoiseSampler.class)
 public abstract class FuckNoiseCavesInParticular {
-	@Inject(at = @At("RETURN"), method = "calculateBaseNoise(IIILnet/minecraft/world/level/levelgen/TerrainInfo;Lnet/minecraft/world/level/levelgen/blending/Blender;)D")
-	private void init(int i, int j, int k, TerrainInfo terrainInfo, Blender blender, CallbackInfoReturnable<Double> info) {
+	@Shadow
+	@Final
+	private boolean isNoiseCavesEnabled;
+
+	@Inject(at = @At("RETURN"), cancellable = true, method = "calculateBaseNoise(IIILnet/minecraft/world/level/levelgen/TerrainInfo;Lnet/minecraft/world/level/levelgen/blending/Blender;)D")
+	private void modifyBaseNoise(int x, int y, int z, TerrainInfo terrainInfo, Blender blender, CallbackInfoReturnable<Double> info) {
+		// this better be in block coordinates
 
 	}
 
+	@Shadow
 	abstract protected double calculateBaseNoise(int i, int j, int k, TerrainInfo terrainInfo, double d, boolean bl, boolean bl2, Blender blender);
 }

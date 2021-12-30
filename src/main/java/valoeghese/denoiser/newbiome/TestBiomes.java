449824e93c0e6ed3c3318a76350eabd1d8126051
valoeghese <valoeghesemods@gmail.com>
2021-12-31 01:06:13 +1300

    THIS should fix vegetation

--- src/main/java/valoeghese/denoiser/newbiome/TestBiomes.java
+++ src/main/java/valoeghese/denoiser/newbiome/TestBiomes.java
@@ -5,9 +5,11 @@ import net.minecraft.data.BuiltinRegistries;
 import net.minecraft.data.worldgen.Carvers;
 import net.minecraft.data.worldgen.biome.OverworldBiomes;
 import net.minecraft.data.worldgen.features.FeatureUtils;
+import net.minecraft.data.worldgen.features.VegetationFeatures;
 import net.minecraft.data.worldgen.placement.MiscOverworldPlacements;
 import net.minecraft.data.worldgen.placement.PlacementUtils;
 import net.minecraft.data.worldgen.placement.TreePlacements;
+import net.minecraft.data.worldgen.placement.VegetationPlacements;
 import net.minecraft.resources.ResourceKey;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
@@ -18,10 +20,13 @@ import net.minecraft.world.level.biome.BiomeGenerationSettings;
 import net.minecraft.world.level.biome.BiomeSpecialEffects;
 import net.minecraft.world.level.biome.MobSpawnSettings;
 import net.minecraft.world.level.levelgen.GenerationStep;
+import net.minecraft.world.level.levelgen.Heightmap;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.WeightedPlacedFeature;
 import net.minecraft.world.level.levelgen.feature.configurations.RandomFeatureConfiguration;
+import net.minecraft.world.level.levelgen.placement.HeightmapPlacement;
+import net.minecraft.world.level.levelgen.placement.InSquarePlacement;
 import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
 import java.util.List;
@@ -31,7 +36,7 @@ public class TestBiomes extends OverworldBiomes {
 
 	private static final ConfiguredFeature<RandomFeatureConfiguration,?> TREES = FeatureUtils.register("denoiser:bushes", Feature.RANDOM_SELECTOR.configured(
 			new RandomFeatureConfiguration(List.of(new WeightedPlacedFeature(TreePlacements.FANCY_OAK_BEES, 0.05f)), TreePlacements.JUNGLE_BUSH)));
-	private static final PlacedFeature TREE_GEN = PlacementUtils.register("denoiser:bushes", TREES.placed(PlacementUtils.countExtra(4, 0.1f, 1)));
+	private static final PlacedFeature TREE_GEN = PlacementUtils.register("denoiser:bushes", TREES.placed(VegetationPlacements.treePlacement(PlacementUtils.countExtra(6, 0.1f, 4))));
 
 	public static final Biome BIOME = new Biome.BiomeBuilder()
 			.biomeCategory(Biome.BiomeCategory.DESERT)
@@ -44,13 +49,14 @@ public class TestBiomes extends OverworldBiomes {
 					.addCarver(GenerationStep.Carving.AIR, Carvers.CANYON)
 					.addFeature(GenerationStep.Decoration.LOCAL_MODIFICATIONS, MiscOverworldPlacements.FOREST_ROCK)
 					.addFeature(GenerationStep.Decoration.SURFACE_STRUCTURES, MiscOverworldPlacements.ICEBERG_BLUE)
+					.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, VegetationPlacements.BAMBOO_LIGHT)
 					.addFeature(GenerationStep.Decoration.VEGETAL_DECORATION, TREE_GEN)
 					.build())
 			.mobSpawnSettings(new MobSpawnSettings.Builder()
 					.addSpawn(MobCategory.CREATURE, new MobSpawnSettings.SpawnerData(EntityType.FOX, 10, 5, 7)) // weight min max
 					.build())
 			.specialEffects(new BiomeSpecialEffects.Builder()
-					.fogColor(0xFF0000)
+					.fogColor(0xCF9F00)
 					.waterColor(0x44BFEF)
 					.waterFogColor(0x00A4A4)
 					.skyColor(calculateSkyColor(0.5f))
